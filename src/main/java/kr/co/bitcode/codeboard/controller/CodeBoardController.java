8c798878bcf593395006dd785bf7b88abc394540
Lee Jeong Hun <wjdgns1155>
2018-07-05 12:06:40 +0900

    rest

--- src/main/java/kr/co/bitcode/codeboard/controller/CodeBoardController.java
+++ src/main/java/kr/co/bitcode/codeboard/controller/CodeBoardController.java
@@ -2,14 +2,27 @@ package kr.co.bitcode.codeboard.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 
 @Controller
 @RequestMapping("/codeboard")
 public class CodeBoardController {
 
-	@RequestMapping("/listBoard.do")
-	public void codeboard() {
+	@RequestMapping(value="/{pageNo}/Board/.do", method=RequestMethod.GET)
+	public void listboard() {
+		
+	}
+	@RequestMapping(value="/Board.do", method=RequestMethod.GET)
+	public void detailboard() {
+		
+	}
+	@RequestMapping(value="/Board.do", method=RequestMethod.PATCH)
+	public void updateboard() {
+		
+	}
+	@RequestMapping(value="/Board.do", method=RequestMethod.DELETE)
+	public void deleteboard() {
 		
 	}
 }
