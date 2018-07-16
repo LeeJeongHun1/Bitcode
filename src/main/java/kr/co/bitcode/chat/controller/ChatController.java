e756eab6c5c8a5c477dbf041ffa50a89b427cfee
JongMin <da6225@gmail.com>
2018-07-16 19:17:06 +0900

    websocket Component 수정

--- src/main/java/kr/co/bitcode/chat/controller/ChatController.java
+++ src/main/java/kr/co/bitcode/chat/controller/ChatController.java
@@ -1,15 +1,33 @@
 package kr.co.bitcode.chat.controller;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
 
 @Controller
 @RequestMapping("/chat")
 public class ChatController {
+	
+	@RequestMapping("/chat.do")
+	public void chat() {
+		
+	}
+	
+	@RequestMapping("/login.do")
+	@ResponseBody
+	public String login(HttpSession session, Member m) {
+		session.setAttribute("user", m);
+		return "success";
+	}
 
-	@RequestMapping(value="/chat.do", method=RequestMethod.GET)
-	public String chat() {
-		return "chat/chat";
+	@RequestMapping("/logout.do")
+	@ResponseBody
+	public String logout(HttpSession session) {
+		session.invalidate();
+		return "success";
 	}
 }
