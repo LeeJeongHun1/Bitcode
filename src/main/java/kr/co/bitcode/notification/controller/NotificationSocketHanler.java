173a538b8c872a20970b87e70120a9c743d89d3f
JongMin <da6225@gmail.com>
2018-07-18 18:49:18 +0900

    users -> chatUsers

--- src/main/java/kr/co/bitcode/notification/controller/NotificationSocketHanler.java
+++ src/main/java/kr/co/bitcode/notification/controller/NotificationSocketHanler.java
@@ -39,14 +39,14 @@ public class NotificationSocketHanler extends TextWebSocketHandler {
 		System.out.println("------------------------------");
 		System.out.println("접속한 사용자 관리 목록");
 		System.out.println("------------------------------");		
-		Set<String> chatKeys = users.keySet();
+		Set<String> chatKeys = chatUsers.keySet();
 		String userList = "userList:";
 		for(String key : chatKeys) {
 			System.out.println(key);
 			userList = userList + key + ":";			
 		}
 		for(String key : chatKeys) {
-			WebSocketSession wss = users.get(key);
+			WebSocketSession wss = chatUsers.get(key);
 			wss.sendMessage(new TextMessage(userList));
 		}
 		
@@ -86,10 +86,10 @@ public class NotificationSocketHanler extends TextWebSocketHandler {
 		System.out.println("보낸 아이디 : " + session.getId());
 		System.out.println("보낸 메세지 : " + message.getPayload());		
 		// 서버에 접속한 모든 사용자에게 메세지 전송하기
-		Set<String> chatKeys = users.keySet();
+		Set<String> chatKeys = chatUsers.keySet();
 		for(String key : chatKeys) {
 			System.out.println(key);
-			WebSocketSession wss = users.get(key);
+			WebSocketSession wss = chatUsers.get(key);
 			wss.sendMessage(
 					new TextMessage(message.getPayload()));
 		};		
